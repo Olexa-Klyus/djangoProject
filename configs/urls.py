377033c729ef12d042348823e404578b31788e61
olexa <olexakljus@gmail.com>
2022-06-28 20:09:03 +0300

    lesson2

--- configs/urls.py
+++ configs/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
 
-
+from cars.views import CarListCreateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
+    # path('admin/', admin.site.urls),
+    path('cars', CarListCreateView.as_view()),
 
 ]
