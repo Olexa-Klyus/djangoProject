2dd000f26d373f248da1a4c8c2b2a5c84ec3c5b3
olexa <olexakljus@gmail.com>
2022-07-01 09:42:12 +0300

    ls2

--- apps/cars/admin.py
+++ apps/cars/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import CarModel
+
 
 # Register your models here.
+# admin.site.register(CarModel)
+
+# або по своєму настроїти адмінку
+@admin.register(CarModel)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('id', 'brand', 'price')
--- apps/cars/models.py
+++ apps/cars/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class CarModel(models.Model):
     class Meta:
         db_table = 'cars'
+        verbose_name = 'Car'
 
     brand = models.CharField(max_length=100)
     year = models.IntegerField()
     price = models.IntegerField()
+
+    def __str__(self):
+        return self.brand
