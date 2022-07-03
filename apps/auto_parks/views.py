38ecb6d14c883d7bcc365d25109811df75ff44cd
olexa <olexakljus@gmail.com>
2022-07-03 16:28:45 +0300

    less4

--- apps/auto_parks/views.py
+++ apps/auto_parks/views.py
@@ -1,4 +1,6 @@
-from rest_framework.generics import ListCreateAPIView, CreateAPIView, RetrieveDestroyAPIView
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, GenericAPIView, RetrieveDestroyAPIView
+from rest_framework.response import Response
 
 from .models import AutoParksModel
 from .serializers import AutoParkSerializer
@@ -15,10 +17,42 @@ class AutoParkRetrieveDestroyView(RetrieveDestroyAPIView):
     serializer_class = AutoParkSerializer
 
 
-class AutoParkAddCarView(CreateAPIView):
+# запис через GenericAPIView
+
+class AutoParkAddCarView(GenericAPIView):
     queryset = AutoParksModel
     serializer_class = CarSerializer
 
-    def perform_create(self, serializer):
+    def post(self, *args, **kwargs):
         auto_park = self.get_object()
-        serializer.save(auto_park_id=auto_park.id)
+        car = self.request.data
+        serializer = self.serializer_class(data=car)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(auto_park=auto_park)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+
+# from rest_framework.generics import ListCreateAPIView, CreateAPIView, RetrieveDestroyAPIView
+#
+# from .models import AutoParksModel
+# from .serializers import AutoParkSerializer
+# from apps.cars.serializers import CarSerializer
+#
+#
+# class AutoParksListCreateView(ListCreateAPIView):
+#     queryset = AutoParksModel.objects.all()
+#     serializer_class = AutoParkSerializer
+#
+#
+# class AutoParkRetrieveDestroyView(RetrieveDestroyAPIView):
+#     queryset = AutoParksModel.objects.all()
+#     serializer_class = AutoParkSerializer
+#
+#
+# class AutoParkAddCarView(CreateAPIView):
+#     queryset = AutoParksModel
+#     serializer_class = CarSerializer
+#
+#     def perform_create(self, serializer):
+#         auto_park = self.get_object()
+#         serializer.save(auto_park_id=auto_park.id)
